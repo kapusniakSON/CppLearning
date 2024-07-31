a9fad415651f0ee8bd2b72ff27998d7537745c55
Kapilo <124464037+kapusniakSON@users.noreply.github.com>
2024-07-31 16:31:06 +0200

    repair gravitiFall program. Add new function.

---
+++ LearnCpp/gravityFall/gravitation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void nowHeight(double height, int second)
+{
+    double gravitation{9.8};
+    double nowHeight{};
+
+    nowHeight = height - gravitation * second * second / 2;
+
+    if (nowHeight > 0)
+    {
+        std::cout << "At " << second << ", the ball is at height: " << nowHeight
+                  << " meters.\n";
+    }
+    else
+    {
+        std::cout << "At " << second << ", the ball is on the ground.\n";
+    }
+}
