9aade59f6f48890858e799f8b3bc0926e88864f2
Kapilo <124464037+kapusniakSON@users.noreply.github.com>
2024-07-31 16:04:43 +0200

    Add program counting numbers letters in name and add age both is insert from user

---
+++ LearnCpp/lenghtOfNameAndAge/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your full name: ";
+    std::string fullName{};
+    std::getline(std::cin >> std::ws, fullName);
+
+    std::cout << "enter your age: ";
+    int age;
+    std::cin >> age;
+
+    // count a fullName length
+    int lengthFullName{static_cast<int>(std::ssize(fullName))};
+
+    std::cout << "Your age + length of name is: " << age + lengthFullName << '\n';
+
+    return 0;
+}
