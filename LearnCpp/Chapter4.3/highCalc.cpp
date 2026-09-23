9fa6ec7e44d8fc71091033c6b10d9a1e987a047e
kapusniakSON <124464037+kapusniakSON@users.noreply.github.com>
2026-09-23 11:28:03 +0200

    add new files

---
+++ LearnCpp/Chapter4.3/highCalc.cpp
@@ -0,0 +1,8 @@
+#include "highCalc.h"
+#include <iostream>
+
+int getTowerHeigh() {
+    std::cout << "Enter the height of the tower in meters: ";
+    int x{};
+    std::cin >> x;
+}
---
+++ LearnCpp/Chapter4.3/highCalc.h
@@ -0,0 +1,6 @@
+#ifndef HIGH_CALC_H
+#define HIGH_CALC_H
+
+int getTowerHeigh();
+
+#endif
